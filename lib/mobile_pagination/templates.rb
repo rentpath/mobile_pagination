a8a1fd7fe81b42c5546237ddf9b930ab3d452633
Joe Dursun <joedursun@gmail.com>
2013-09-17 10:05:55 -0400

    [JD] Generate pagination links via helper method

--- lib/mobile_pagination/templates.rb
+++ lib/mobile_pagination/templates.rb
@@ -2,27 +2,35 @@ module MobilePagination
   module Templates
 
     def first_page_html
-      "<li>
-        <a title='First Page' href='#{first_page_link}'>First Page</a> |
-      </li>"
+      generate_pagination_link(
+          title: 'First Page',
+          href: first_page_link,
+          text: 'First Page'
+      )
     end
 
     def previous_page_html
-      "<li>
-        <a title='Previous Page' href='#{previous_page_link}'>Previous Page</a> |
-      </li>"
+      generate_pagination_link(
+          title: 'Previous Page',
+          href: previous_page_link,
+          text: 'Previous Page'
+      )
     end
 
     def next_page_html
-      "<li>
-        <a title='Next Page' href='#{next_page_link}'>Next Page</a> |
-      </li>"
+      generate_pagination_link(
+          title: 'Next Page',
+          href: next_page_link,
+          text: 'Next Page'
+      )
     end
 
     def last_page_html
-      "<li>
-        <a title='Last Page' href='#{last_page_link}'>Last Page</a>
-      </li>"
+      generate_pagination_link(
+          title: 'Last Page',
+          href: last_page_link,
+          text: 'Last Page'
+      )
     end
 
     def html
@@ -34,6 +42,14 @@ module MobilePagination
         markup << last_page_html     if last_page?
       end
     end
+
+    private
+
+      def generate_pagination_link(opts)
+        "<li>
+          <a title='#{opts[:title]}' href='#{opts[:href]}'>#{opts[:text]}</a>
+        </li>"
+      end
 
   end
 end
