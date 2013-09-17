250894416e411c30c8cca4e76feae1f313c61c61
Luke Fender <lfender6445@gmail.com>
2013-09-17 10:30:47 -0400

    [LF] [000] fix indentation, remove private

--- lib/mobile_pagination/templates.rb
+++ lib/mobile_pagination/templates.rb
@@ -2,35 +2,27 @@ module MobilePagination
   module Templates
 
     def first_page_html
-      generate_pagination_link(
-          title: 'First Page',
-          href: first_page_link,
-          text: 'First Page'
-      )
+      generate_pagination_link title: 'First Page',
+                               href: first_page_link,
+                               text: 'First Page'
     end
 
     def previous_page_html
-      generate_pagination_link(
-          title: 'Previous Page',
-          href: previous_page_link,
-          text: 'Previous Page'
-      )
+      generate_pagination_link title: 'Previous Page',
+                               href: previous_page_link,
+                               text: 'Previous Page'
     end
 
     def next_page_html
-      generate_pagination_link(
-          title: 'Next Page',
-          href: next_page_link,
-          text: 'Next Page'
-      )
+      generate_pagination_link title: 'Next Page',
+                               href: next_page_link,
+                               text: 'Next Page'
     end
 
     def last_page_html
-      generate_pagination_link(
-          title: 'Last Page',
-          href: last_page_link,
-          text: 'Last Page'
-      )
+      generate_pagination_link title: 'Last Page',
+                               href: last_page_link,
+                               text: 'Last Page'
     end
 
     def html
@@ -43,13 +35,11 @@ module MobilePagination
       end
     end
 
-    private
-
-      def generate_pagination_link(opts)
-        "<li>
-          <a title='#{opts[:title]}' href='#{opts[:href]}'>#{opts[:text]}</a>
-        </li>"
-      end
+    def generate_pagination_link(opts)
+      "<li>
+        <a title='#{opts[:title]}' href='#{opts[:href]}'>#{opts[:text]}</a>
+      </li>"
+    end
 
   end
 end
